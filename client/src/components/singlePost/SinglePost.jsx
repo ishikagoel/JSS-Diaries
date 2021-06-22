8285898bb2d4572cda753c89793c56730374fc7e
beingbravado <sharmagyanesh199@gmail.com>
2021-06-23 00:09:18 +0530

    context api

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useLocation, Link } from "react-router-dom"
+import { useLocation, Link } from "react-router-dom";
 import "./singlePost.css"
 
 export default function SinglePost() {
